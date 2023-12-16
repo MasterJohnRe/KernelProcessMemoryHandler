a9463d56588be08f919f74a6a41e058ecfbaaced
John <masterjohn553@gmail.com>
2023-12-16 12:02:55 +0200

    basic functionallity of writing memory to process works, including protected memory

---
+++ WriteProcessMemory/Memory.cpp
@@ -0,0 +1,8 @@
+#include "Memory.h"
+
+NTSTATUS KernelWriteVirtualMemory(PEPROCESS process, PVOID sourceAddress, PVOID targetAddress, SIZE_T size) {
+	SIZE_T Bytes;
+	NTSTATUS copyVirtualMemoryStatus = MmCopyVirtualMemory(PsGetCurrentProcess(), sourceAddress, process, targetAddress, size, KernelMode, &Bytes);;
+	KdPrint(("copied %Iu bytes", Bytes));
+	return copyVirtualMemoryStatus;
+}
